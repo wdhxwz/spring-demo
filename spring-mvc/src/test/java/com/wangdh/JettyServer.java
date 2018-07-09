f7cc6caa7381d5e5fd4d5a5b711dfe2583ecd4a3
wangdh <1366678737@qq.com>
2018-07-09 15:33:06 +0800

    添加测试

--- spring-mvc/src/test/java/com/wangdh/JettyServer.java
+++ spring-mvc/src/test/java/com/wangdh/JettyServer.java
@@ -2,6 +2,6 @@ package com.wangdh;
 
 public class JettyServer {
     public static void main(String[] args) throws Exception{
-
+        com.wangdh.common.test.jetty.JettyServer.start(9090);
     }
 }
